76eacb518dea647989adaf996955a3d84b7397e2
吕向杰 <lvxiangjie@yangshipin.cn>
2022-03-10 15:34:59 +0800

    ddd

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '7e62d9831cb61f8f2b4f813062fbf544a9bd40bc',
         'name' => 'lvxiangjie/aw',
         'dev' => true,
     ),
     'versions' => array(
         'lvxiangjie/aw' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '7e62d9831cb61f8f2b4f813062fbf544a9bd40bc',
             'dev_requirement' => false,
         ),
     ),
