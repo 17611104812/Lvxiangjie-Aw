b7eb524dcbe7eba584c30203daab71b702624d94
吕向杰 <lvxiangjie@yangshipin.cn>
2022-03-10 15:00:50 +0800

    提交

---
+++ src/Lvxiangjie/Aw/Reader/InvalidDatabaseException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Lvxiangjie\Aw\Reader;
+
+use Exception;
+
+/**
+ * This class should be thrown when unexpected data is found in the database.
+ */
+class InvalidDatabaseException extends Exception
+{
+}
